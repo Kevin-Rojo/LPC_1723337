2523f331b012a1f0c4c7550bbdc9d1c2530839fd
Kevin-Rojo <61994525+Kevin-Rojo@users.noreply.github.com>
2021-03-19 19:37:43 -0600

    Create ScriptPowershell.ps1
    Practica 3

---
+++ Practica3/ScriptPowershell.ps1
@@ -0,0 +1,23 @@
+﻿function copiar-txt{
+    param([String]$directorio)
+    $files = Get-ChildItem -Path $directorio
+    $num = 1 
+
+    while( $num -lt $files.Length ){
+        if([String]$files[$num].GetType() -eq "System.IO.FileInfo"){
+            $files_txt = $files[$num]
+            [BOOL]$files_validation = $true
+        } 
+        $num++
+    }
+    if($files_validation){
+        New-Item -Path 'C:\Archivos-Robados' -ItemType Directory
+        foreach($file in $files_txt){
+            Copy-Item -Path C:\$file -Destination C:\Archivos-Robados
+        }
+    }else{
+        Write-Host "No se encontraron Archivos de texto"
+    }
+}
+
+copiar-txt c://
